17e8c386ccd845b2877debf06282fa6b557b5910
tayqiter00 <tayqiter00@gmail.com>
2024-12-04 15:37:29 +0800

    Should ACTUALLY work now

--- srcs/requirements/wordpress/tools/install_wordpress.sh
+++ srcs/requirements/wordpress/tools/install_wordpress.sh
@@ -16,9 +16,9 @@ sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config.php
 sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
 
 wp core install --url=$WP_URL/ --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-wp user create $WP_USR --role=author --user_pass=$WP_PASS --allow-root
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
 
 wp theme install astra --activate --allow-root
 wp plugin update --all --allow-root
 
-/usr/sbin/php-fpm7.3 -F
+/usr/sbin/php-fpm7.4 -F
