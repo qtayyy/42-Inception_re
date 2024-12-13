b1b3a94589a5f927f09c4393eb3b6ae4fd17b446
tayqiter00 <tayqiter00@gmail.com>
2024-12-13 15:07:08 +0800

    simplify

--- srcs/requirements/wordpress/tools/install_wordpress.sh
+++ srcs/requirements/wordpress/tools/install_wordpress.sh
@@ -18,7 +18,4 @@ sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
 wp core install --url=$WP_URL/ --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
 
-wp theme install astra --activate --allow-root
-wp plugin update --all --allow-root
-
 /usr/sbin/php-fpm7.4 -F
