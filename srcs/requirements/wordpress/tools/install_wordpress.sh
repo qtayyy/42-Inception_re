775d5bd1b285c185bcd9f37bf6ed01cc254adcf2
tayqiter00 <tayqiter00@gmail.com>
2025-03-05 01:44:58 +0800

    Remove unnecessary EXPOSE in Dockerfiles

--- srcs/requirements/wordpress/tools/install_wordpress.sh
+++ srcs/requirements/wordpress/tools/install_wordpress.sh
@@ -15,7 +15,7 @@ sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
 sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config.php
 sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
 
-wp core install --url=$WP_URL/ --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
 
 /usr/sbin/php-fpm7.4 -F
